8bbc8e7a2da2a2b44e3a2d0f27178241d6cb8391
Rahul Harsh <rahulharsh017@gmail.com>
2024-01-19 03:50:16 +0530

    08miniContext

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import UserContextProvider from "./Context/UseContextProvider"
+import Login from "./components/Login"
+import Profile from "./components/Profile"
+import './App.css'
+
+function App() {
+
+  return (
+<UserContextProvider>
+<h1>Context APi</h1>
+<Login/>
+<Profile/>
+</UserContextProvider>
+      
+ 
+  )
+}
+
+export default App
